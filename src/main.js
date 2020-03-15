0eb6c79c5a15e23dde4b2ff889f4830ab5c472d0
meiyi zheng <524875323@qq.com>
2020-03-15 11:09:25 +0800

    vant的安装和使用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入vant
+import 'vant/lib/index.css' // 引入vant的css文件
+Vue.use(Vant) // 全局注册vant
 
 Vue.config.productionTip = false
 
