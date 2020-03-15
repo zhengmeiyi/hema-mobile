5c6c8c94be49f4bae842d7defbc75355bb8cdd83
meiyi zheng <524875323@qq.com>
2020-03-15 21:47:29 +0800

    本地存储token

---
+++ src/utils/auth.js
@@ -0,0 +1,19 @@
+/**
+ * 专门处理token的读写和删除
+ */
+const USER_TOKEN = 'heima-toutiao-mobile-94' // 专门用来存储用户信息
+
+// 设置用户的token信息
+export function setUser (user) {
+  window.localStorage.getItem(USER_TOKEN, JSON.stringify(user))
+}
+
+// 获取用户的token
+export function getUser () {
+  return JSON.parse(window.localStorage.getItem(USER_TOKEN))
+}
+
+// 删除token
+export function delUser () {
+  window.localStorage.removeItem(USER_TOKEN)
+}
