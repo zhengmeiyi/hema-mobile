9816e0c509bbc22d8dad9fb66e4051da9ccf458a
meiyi zheng <524875323@qq.com>
2020-03-17 19:16:30 +0800

    vuex状态管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
+/**
+ * 专门处理vuex中的模块
+ *
+ */
+
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: auth.getUser() // 放置需要共享的状态。token，如果缓存有token,读取缓存的token
   },
-  mutations: {
+  mutations: { // 改token只能通过mutation
+    updateUser (state, payload) {
+      state.user = payload.user // 定义载荷中的user数据给state
+      auth.setUser(payload.user) // 把最新的数据存入本地缓存
+    },
+    delUser (state) { // 删除token
+      state.user = {}
+      auth.delUser() // 删除本地缓存的token
+    }
   },
   actions: {
   },
