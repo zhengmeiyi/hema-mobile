c101972af5c4e4cc5cc06a2b78ae067718abc2e0
meiyi zheng <524875323@qq.com>
2020-03-17 20:36:02 +0800

    登录页-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
     <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
+    <can-cell-group>
+      <van-field label="手机号：" placeholder="请输入手机号">
+      </van-field>
+      <van-field label="验证码：" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="primary">发送验证码
+        </van-button>
+      </van-field>
+      <div class="login-box">
+        <van-button type="info" round size="small" block> 登录</van-button>
+      </div>
+    </can-cell-group>
   </div>
 </template>
 
@@ -11,5 +22,7 @@ export default {
 </script>
 
 <style>
-
+.login-box{
+  padding: 20px;
+}
 </style>
