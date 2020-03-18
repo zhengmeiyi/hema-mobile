a37fcd6fd8a9de3ba62ef2fbd65dda729aa676e2
meiyi zheng <524875323@qq.com>
2020-03-18 18:52:23 +0800

    修改vant主题蓝色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant
-import 'vant/lib/index.css' // 引入vant的css文件
+import 'vant/lib/index.less' // 引入vant的css文件
 import 'amfe-flexible' // 运行时根据屏幕大小决定font-size大小
 import './styles/index.less'
 Vue.use(Vant)// 全局注册vant
