56b45f41308c25e6e51ba6740a31bd64f8bcd04e
meiyi zheng <524875323@qq.com>
2020-03-21 19:13:05 +0800

    时间格式处理

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,9 +1,20 @@
+// -------------------------封装一个相对时间函数
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+// 转化时间到相对时间
+function relTime (date) {
+  return dayjs().locale('zh-cn').from(date)
+}
+
 export default {
   install (Vue) {
     Vue.prototype.$znotify = (params) => Vue.prototype.$notify({
       duration: 800, ...params
     })
     Vue.prototype.$sleep = sleep
+    Vue.filter('relTime', relTime) // 把相对时间注册到过滤器
   }
 }
 // 休眠函数
@@ -14,3 +25,4 @@ function sleep (time = 500) {
     setTimeout(() => resolve(), time)
   })
 }
+// 时间函数
