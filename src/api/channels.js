cf7c8098c01288c4390422a9e361ec37847d16e0
meiyi zheng <524875323@qq.com>
2020-03-22 21:05:02 +0800

    添加我的频道API

--- src/api/channels.js
+++ src/api/channels.js
@@ -57,3 +57,15 @@ export function delChannel (id) {
     }
   })
 }
+/**
+ * 添加频道
+ */
+export function addChannel (channel) {
+  return new Promise(function (resolve, reject) {
+    const key = store.state.user.token ? CACHE_CHANNEL_V : CACHE_CHANNEL_T
+    const channels = JSON.parse(localStorage.getItem(key))
+    channels.pish(channel)
+    localStorage.setItem(key, JSON.stringify(channels))
+    resolve()
+  })
+}
