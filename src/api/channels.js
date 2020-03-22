3f1671b7ea9f7c9c10bd09e8df30da5c51b46d57
meiyi zheng <524875323@qq.com>
2020-03-22 19:57:46 +0800

    渲染可选频道

--- src/api/channels.js
+++ src/api/channels.js
@@ -1,6 +1,17 @@
 import request from '@/utils/request'
+/**
+ * 获取我的频道
+ */
 export function getMyChannels () {
   return request({
     url: '/user/channels'
+  })
+}
+/**
+ * 获取所有频道
+ */
+export function getAllChannels () {
+  return request({
+    url: '/channels'
   })
 }
--- src/views/home/component/channel-edit.vue
+++ src/views/home/component/channel-edit.vue
@@ -17,8 +17,8 @@
     <div class="channel">
       <div class="tit">可选频道：</div>
       <van-grid class="van-hairline--left">
-        <van-grid-item v-for="index in 8" :key="index">
-          <span class="f12">频道{{index}}</span>
+        <van-grid-item v-for="channel in optionalAllChannels" :key="channel.id">
+          <span class="f12">{{channel.name}}</span>
           <van-icon class="btn" name="plus"></van-icon>
         </van-grid-item>
       </van-grid>
@@ -27,10 +27,12 @@
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channels'
 export default {
   data () {
     return {
-      editing: false
+      editing: false,
+      allChannels: []
     }
   },
   props: {
@@ -38,6 +40,20 @@ export default {
       required: true,
       type: Array,
       default: () => []
+    }
+  },
+  methods: {
+    async getAllChannels () {
+      const data = await getAllChannels()
+      this.allChannels = data.channels
+    }
+  },
+  created () {
+    this.getAllChannels()
+  },
+  computed: {
+    optionalAllChannels () {
+      return this.allChannels.filter(item => !this.channels.some(o => o.id === item.id))
     }
   }
 }
