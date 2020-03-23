559460343a51e34fd3d5b5304957c917552293d2
meiyi zheng <524875323@qq.com>
2020-03-23 20:21:11 +0800

    搜索中心-历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,21 +3,21 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
+    <van-search @search="onSearch" v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
     <van-cell-group class="suggest-box" >
       <van-cell icon="search" v-if="q">
         <span>java</span>
       </van-cell>
     </van-cell-group>
     <div class="history-box" v-if="!q">
-      <div class="head">
+      <div class="head" v-if="historyList.length">
         <span>历史记录</span>
-        <van-icon name="delete"></van-icon>
+        <van-icon name="delete" @click="clear"></van-icon>
       </div>
-      <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+      <van-cell-group v-for="(item,index) in historyList" :key="index">
+        <van-cell @click="toSearchResult(item)">
+          <a class="word_btn">{{item}}</a>
+          <van-icon @click.stop="delHistory(index)" class="close_btn" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -25,11 +25,42 @@
 </template>
 
 <script>
+const key = 'hema-mobile-history'
 export default {
   name: 'search',
   data () {
     return {
-      q: ''
+      q: '',
+      historyList: JSON.parse(localStorage.getItem(key) || '[]')
+    }
+  },
+  methods: {
+    delHistory (index) { // 删除当前历史记录
+      this.historyList.splice(index, 1) // 删除当前元素
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 存入缓存
+    },
+    toSearchResult (text) { // 点击历史记录跳转到搜索结果
+      this.$router.push({ path: '/search/result', query: { q: text } })
+    },
+    async clear () { // 清空历史记录
+      try {
+        await this.$dialog.confirm({
+          title: '提示',
+          message: '确定要清空历史记录吗？'
+        })
+        this.historyList = [] // 清空列表
+        localStorage.setItem(key, '[]') // 清空缓存
+      } catch (error) {
+        // 什么都不用做
+      }
+    },
+    onSearch () { // 搜索
+    // 如果搜索内容为空，则返回
+      if (!this.q) return
+      this.historyList.push(this.q) // 添加到历史记录列表
+      this.historyList = Array.from(new Set(this.historyList)) // 去重
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重置缓存
+      this.$router.push({ path: '/search/result', query: { q: this.q } }) // 跳转到搜索结果并携带数据
     }
   }
 }
