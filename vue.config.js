ffb9dc7aaa5a2d5a2597dc1a67a20b53ac0c7254
meiyi zheng <524875323@qq.com>
2020-03-29 22:02:33 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,11 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'
 }
